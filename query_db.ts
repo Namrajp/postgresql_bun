9917d7bb376bbf268f95b298987fd83d1cd26563
namrajp <active99raj@mgmail.com>
2026-03-03 14:40:45 +1100

    join tables tasks and categories to fetch all tasks

--- query_db.ts
+++ query_db.ts
@@ -5,7 +5,26 @@ try {
     SELECT * FROM categories;
   `;
 
-  console.log(categories);
+  console.log(categories.map((category: any) => category.name));
+
+  //   const tasks = await sql`
+  //     SELECT * FROM tasks;
+  //   `;
+
+  //   console.log(tasks);
+
+  const tasks = await sql`
+
+    SELECT
+        tasks.*,
+        categories.name AS category_name
+    FROM
+        tasks
+    JOIN
+        categories ON tasks.category_id = categories.id;
+  `;
+
+  console.log(tasks);
 } catch (error: any) {
   console.log("Error: " + error.message);
 }
